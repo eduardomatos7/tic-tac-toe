e8d8cff1f4e445063cdb8ca63a131d70010d509c
eduardomatos7 <eduardo.matoss@ufpe.br>
2024-10-05 13:58:04 -0300

    implementing player turn logic after input

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,4 +1,4 @@
-const nomeJogadorX = document.getElementById('player1')
+/*const nomeJogadorX = document.getElementById('player1')
 const nomeJogadorO = document.getElementById('player2')
 const vez = document.getElementById('vez')
 const cellElements = document.querySelectorAll('.area')
@@ -19,6 +19,28 @@ cellElements.forEach(function(element){
         }
     })
 })
+*/
+const BoardRegions = document.querySelectorAll('.area')
+let vBoard = []
+let turnPlayer = ''
 
+function updateTitle() {
+    const playerInput = document.getElementById(turnPlayer)
+    document.getElementById('turnPlayer').innerText = playerInput.value
+}
 
+function initializeGame(){
+    vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+  turnPlayer = 'player1'
+  document.querySelector('h2').innerHTML = ' Vez de: <span id="turnPlayer"></span>'
+  updateTitle()
+  BoardRegions.forEach(function(element){
+    element.classList.remove('win')
+    element.innerText = ''
+    element.addEventListener('click', handleBoardClick)
+  })
+    
+}
+
+document.getElementById('start').addEventListener('click', initializeGame)
 
