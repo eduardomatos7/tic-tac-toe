912ecd057d7b7f03807c6e62751b2c5f7fe5deee
eduardomatos7 <eduardo.matoss@ufpe.br>
2024-10-07 20:05:33 -0300

    function update h2 ve of the next player

--- src/JS/index.js
+++ src/JS/index.js
@@ -43,6 +43,28 @@ function initializeGame(){
         
 }
 
+// Verifica se existem três regiões iguais em sequência e devolve as regiões
+function getWinRegions() {
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+      winRegions.push("0.0", "0.1", "0.2")
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+      winRegions.push("1.0", "1.1", "1.2")
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+      winRegions.push("2.0", "2.1", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+      winRegions.push("0.0", "1.0", "2.0")
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+      winRegions.push("0.1", "1.1", "2.1")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+      winRegions.push("0.2", "1.2", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+      winRegions.push("0.0", "1.1", "2.2")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+      winRegions.push("0.2", "1.1", "2.0")
+    return winRegions
+  }
+
 function disableRegion(element){
     element.style.cursor = 'not-allowed' //mostrar bloqueio ao tentar clicar na regiao ja marcada
     element.removeEventListener('click', handleBoardClick)
@@ -56,24 +78,27 @@ function handleBoardClick(ev){
     const rowColumnPair = region.split('.')
     const row = rowColumnPair[0]
     const column = rowColumnPair[1]
-    if (span.innerText === ''){
-        if (turnPlayer === 'player1'){
-            span.innerHTML = 'X'
-            vBoard[row][column] = 'X'
-            turnPlayer = 'player2'
-            console.log(turnPlayer)
     
-        }else{
-            span.innerHTML = 'O'
-            turnPlayer = 'player1'
-            vBoard[row][column] = 'O'
-        }
-        console.clear()
-        console.table(vBoard)
-        if (span !== ''){
-            disableRegion(span)}
+    if (turnPlayer === 'player1'){
+        span.innerHTML = 'X'
+        vBoard[row][column] = 'X'
+        console.log(turnPlayer)
 
-
+    }else{
+        span.innerHTML = 'O'
+        vBoard[row][column] = 'O'
+    }
+    console.clear()
+    console.table(vBoard)
+    disableRegion(span)
+    const winRegions = getWinRegions()
+    if (winRegions.length > 0){
+        console.log("Venceu")
+    }else if (vBoard.flat().includes('')){
+        turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+        updateTitle()
+    }else{
+        document.querySelector('h2').innerHTML = 'Empate!'
     }
     
 }
